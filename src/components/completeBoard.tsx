0a55ad5e404ac24e3ca52eb6ccc62fae03e10593
Swadhin Kumar Senapati <102758279+SKumar425@users.noreply.github.com>
2025-01-07 22:39:08 +0530

    edit functionality

--- src/components/completeBoard.tsx
+++ src/components/completeBoard.tsx
@@ -3,12 +3,14 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteTask, rearrangeTasks } from "../store/taskSlice";
 import { RootState } from "../store"; // Import RootState type
-
 import { Reorder } from "framer-motion";
+import Modal from "./modal";
 
 const CompleteBoard: React.FC = () => {
   const [menuTaskId, setMenuTaskId] = useState<string | null>(null);
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  //@ts-ignore
+  const [taskToEdit, setTaskToEdit] = useState<Task | null>(null);
   const dispatch = useDispatch();
   const tasks = useSelector((state: RootState) => state.tasks.tasks); // Type the state using RootState
 
@@ -31,70 +33,82 @@ const CompleteBoard: React.FC = () => {
     dispatch(rearrangeTasks({ reorderedTasks: updatedTasks }));
   };
 
-  return (
-    <div className="w-1/3 flex flex-col gap-4 rounded-xl bg-[#58575112] p-[12px] h-max">
-      <div className="w-max bg-[#CEFFCC] rounded-md text-[#000000] text-[14px] font-[600] leading-[22px] p-2">
-        COMPLETED
-      </div>
-      <div className="w-full flex flex-col">
-        <div className="w-full flex flex-col gap-2">
-          {tasks.filter((task) => task.status === "complete").length > 0 ? (
-            <Reorder.Group
-              as="div"
-              values={tasks.filter((task) => task.status === "complete")}
-              onReorder={handleReorder}
-              className="w-full flex flex-col gap-1"
-            >
-              {tasks
-                .filter((task) => task.status === "complete")
-                .map((task) => (
-                  <Reorder.Item
-                    key={task.id}
-                    value={task}
-                    className="w-full flex text-base text-[#000000] font-[700] items-center hover:shadow-2xl border border-gray-500 rounded-md bg-[#FFFFFF] p-4"
-                  >
-                    <div className="w-full">{task.title}</div>
+  //@ts-ignore
+  const handleEditTask = (task: Task) => {
+    setTaskToEdit(task);
+    setIsModalOpen(true);
+    setMenuTaskId(null);
+  };
 
-                    <div className="relative ">
-                      <button
-                        className="text-gray-500 hover:text-gray-700 text-2xl font-bold pb-2"
-                        onClick={() => handleMenuToggle(task.id)}
-                      >
-                        ...
-                      </button>
-                      {menuTaskId === task.id && (
-                        <div className="absolute right-5 top-0 mt-1 w-32 bg-white border border-gray-300 rounded-md shadow-md z-50">
-                          <button
-                            className="block w-full text-left p-2 hover:bg-gray-100"
-                            onClick={() => {
-                              setMenuTaskId(null);
-                            }}
-                          >
-                            Edit
-                          </button>
-                          <button
-                            className="block w-full text-left p-2 text-red-600 hover:bg-gray-100"
-                            onClick={() => {
-                              handleDeleteTask(task.id);
-                              setMenuTaskId(null);
-                            }}
-                          >
-                            Delete
-                          </button>
-                        </div>
-                      )}
-                    </div>
-                  </Reorder.Item>
-                ))}
-            </Reorder.Group>
-          ) : (
-            <div className="w-full p-2 text-center text-sm text-[#000000] font-[500]">
-              No tasks available
-            </div>
-          )}
+  return (
+    <>
+      <div className="w-1/3 flex flex-col gap-4 rounded-xl bg-[#58575112] p-[12px] h-max">
+        <div className="w-max bg-[#CEFFCC] rounded-md text-[#000000] text-[14px] font-[600] leading-[22px] p-2">
+          COMPLETED
+        </div>
+        <div className="w-full flex flex-col">
+          <div className="w-full flex flex-col gap-2">
+            {tasks.filter((task) => task.status === "complete").length > 0 ? (
+              <Reorder.Group
+                as="div"
+                values={tasks.filter((task) => task.status === "complete")}
+                onReorder={handleReorder}
+                className="w-full flex flex-col gap-1"
+              >
+                {tasks
+                  .filter((task) => task.status === "complete")
+                  .map((task) => (
+                    <Reorder.Item
+                      key={task.id}
+                      value={task}
+                      className="w-full flex text-base text-[#000000] font-[700] items-center hover:shadow-2xl border border-gray-500 rounded-md bg-[#FFFFFF] p-4"
+                    >
+                      <div className="w-full">{task.title}</div>
+
+                      <div className="relative ">
+                        <button
+                          className="text-gray-500 hover:text-gray-700 text-2xl font-bold pb-2"
+                          onClick={() => handleMenuToggle(task.id)}
+                        >
+                          ...
+                        </button>
+                        {menuTaskId === task.id && (
+                          <div className="absolute right-5 top-0 mt-1 w-32 bg-white border border-gray-300 rounded-md shadow-md z-50">
+                            <button
+                              className="block w-full text-left p-2 hover:bg-gray-100"
+                              onClick={() => handleEditTask(task)}
+                            >
+                              Edit
+                            </button>
+                            <button
+                              className="block w-full text-left p-2 text-red-600 hover:bg-gray-100"
+                              onClick={() => {
+                                handleDeleteTask(task.id);
+                                setMenuTaskId(null);
+                              }}
+                            >
+                              Delete
+                            </button>
+                          </div>
+                        )}
+                      </div>
+                    </Reorder.Item>
+                  ))}
+              </Reorder.Group>
+            ) : (
+              <div className="w-full p-2 text-center text-sm text-[#000000] font-[500]">
+                No tasks available
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        task={taskToEdit}
+      />
+    </>
   );
 };
 
--- src/components/completed.tsx
+++ src/components/completed.tsx
@@ -4,10 +4,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { deleteTask, rearrangeTasks } from "../store/taskSlice";
 import { RootState } from "../store"; // Import RootState type
 import { Reorder } from "framer-motion";
+import Modal from "./modal";
 
 const Complete: React.FC = () => {
   const [menuTaskId, setMenuTaskId] = useState<string | null>(null);
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  //@ts-ignore
+  const [taskToEdit, setTaskToEdit] = useState<Task | null>(null);
   const dispatch = useDispatch();
   const tasks = useSelector((state: RootState) => state.tasks.tasks); // Type the state using RootState
 
@@ -23,7 +26,6 @@ const Complete: React.FC = () => {
   const handleReorder = (reorderedInProgressTasks: Task[]) => {
     const allTasks = tasks;
 
-    
     const otherTasks = allTasks.filter((task) => task.status !== "complete");
 
     const updatedTasks = [...otherTasks, ...reorderedInProgressTasks];
@@ -31,74 +33,87 @@ const Complete: React.FC = () => {
     dispatch(rearrangeTasks({ reorderedTasks: updatedTasks }));
   };
 
+  //@ts-ignore
+  const handleEditTask = (task: Task) => {
+    setTaskToEdit(task);
+    setIsModalOpen(true);
+    setMenuTaskId(null);
+  };
+
   return (
-    <div className="w-full flex flex-col rounded-md bg-[#0000001A]">
-      <div className="w-full bg-[#CEFFCC] rounded-t-md text-[#000000] text-[16px] font-[600] leading-[22px] py-3 px-4">
-      COMPLETED
-      </div>
-      <div className="w-full flex flex-col">
-        <div className="w-full flex flex-col gap-1 p-4">
-          {tasks.filter((task) => task.status === "complete").length > 0 ? (
-            <Reorder.Group
-              as="div"
-              values={tasks.filter((task) => task.status === "complete")}
-              onReorder={handleReorder}
-              className="w-full flex flex-col gap-1"
-            >
-              {tasks
-                .filter((task) => task.status === "complete")
-                .map((task) => (
-                  <Reorder.Item
-                    key={task.id}
-                    value={task}
-                    className="w-full flex text-sm text-[#000000] font-[500] items-center hover:shadow-2xl border border-transparent hover:border-gray-500 rounded-md"
-                  >
-                    <div className="p-2 w-1/4">{task.title}</div>
-                    <div className="p-2 w-1/4">
-                      {task.dueDate ? task.dueDate : "No Due Date"}
-                    </div>
-                    <div className="p-2 w-1/4">{task.status}</div>
-                    <div className="p-2 w-1/4">{task.category}</div>
-                    <div className="p-2 relative w-[100px]">
-                      <button
-                        className="text-gray-500 hover:text-gray-700 text-2xl font-bold flex items-center justify-center"
-                        onClick={() => handleMenuToggle(task.id)}
-                      >
-                        ...
-                      </button>
-                      {menuTaskId === task.id && (
-                        <div className="absolute right-0 top-full mt-1 w-32 bg-white border border-gray-300 rounded-md shadow-md z-50">
-                          <button
-                            className="block w-full text-left p-2 hover:bg-gray-100"
-                            onClick={() => {
-                              setMenuTaskId(null);
-                            }}
-                          >
-                            Edit
-                          </button>
-                          <button
-                            className="block w-full text-left p-2 text-red-600 hover:bg-gray-100"
-                            onClick={() => {
-                              handleDeleteTask(task.id);
-                              setMenuTaskId(null);
-                            }}
-                          >
-                            Delete
-                          </button>
-                        </div>
-                      )}
-                    </div>
-                  </Reorder.Item>
-                ))}
-            </Reorder.Group>
-          ) : (
-            <div className="w-full p-2 text-center text-sm text-[#000000] font-[500]">
-              No tasks available
-            </div>
-          )}
+    <>
+      <div className="w-full flex flex-col rounded-md bg-[#0000001A]">
+        <div className="w-full bg-[#CEFFCC] rounded-t-md text-[#000000] text-[16px] font-[600] leading-[22px] py-3 px-4">
+          COMPLETED
+        </div>
+        <div className="w-full flex flex-col">
+          <div className="w-full flex flex-col gap-1 p-4">
+            {tasks.filter((task) => task.status === "complete").length > 0 ? (
+              <Reorder.Group
+                as="div"
+                values={tasks.filter((task) => task.status === "complete")}
+                onReorder={handleReorder}
+                className="w-full flex flex-col gap-1"
+              >
+                {tasks
+                  .filter((task) => task.status === "complete")
+                  .map((task) => (
+                    <Reorder.Item
+                      key={task.id}
+                      value={task}
+                      className="w-full flex text-sm text-[#000000] font-[500] items-center hover:shadow-2xl border border-transparent hover:border-gray-500 rounded-md"
+                    >
+                      <div className="p-2 w-1/4">{task.title}</div>
+                      <div className="p-2 w-1/4">
+                        {task.dueDate ? task.dueDate : "No Due Date"}
+                      </div>
+                      <div className="p-2 w-1/4">{task.status}</div>
+                      <div className="p-2 w-1/4">{task.category}</div>
+                      <div className="p-2 relative w-[100px]">
+                        <button
+                          className="text-gray-500 hover:text-gray-700 text-2xl font-bold flex items-center justify-center"
+                          onClick={() => handleMenuToggle(task.id)}
+                        >
+                          ...
+                        </button>
+                        {menuTaskId === task.id && (
+                          <div className="absolute right-0 top-full mt-1 w-32 bg-white border border-gray-300 rounded-md shadow-md z-50">
+                            <button
+                              className="block w-full text-left p-2 hover:bg-gray-100"
+                              onClick={() => handleEditTask(task)}
+                            >
+                              Edit
+                            </button>
+                            <button
+                              className="block w-full text-left p-2 text-red-600 hover:bg-gray-100"
+                              onClick={() => {
+                                handleDeleteTask(task.id);
+                                setMenuTaskId(null);
+                              }}
+                            >
+                              Delete
+                            </button>
+                          </div>
+                        )}
+                      </div>
+                    </Reorder.Item>
+                  ))}
+              </Reorder.Group>
+            ) : (
+              <div className="w-full p-2 text-center text-sm text-[#000000] font-[500]">
+                No tasks available
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        task={taskToEdit}
+      />
+    </>
   );
 };
 
